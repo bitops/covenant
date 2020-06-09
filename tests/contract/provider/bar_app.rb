52981b7875d7678d65719c36b013c8edea0f54bd
Sebastian Wittenkamp <sebastian@bitops.io>
2020-06-09 16:09:09 -0700

    get tests running

--- tests/contract/provider/bar_app.rb
+++ tests/contract/provider/bar_app.rb
@@ -3,18 +3,11 @@ class BarApp
     status = 200
     headers = {'Content-Type' => 'application/json'}
     body = {
-      "company": "My Company",
-      "factories": [
-        {
-          "location": "Sydney",
-          "capacity": 5
-        },
-        {
-          "location": "Sydney",
-          "geographicCoords": "-0.145,1.4445",
-          "capacity": 5,
+      "data": {
+        "main": {
+          "answer" => "99"
         }
-      ]
+      }
     }.to_json
     [status, headers, [body]]
   end

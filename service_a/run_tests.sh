771ea8b15284effa5d3b46011ddf3445949bc9a5
Sebastian Wittenkamp <sebastian@bitops.io>
2020-06-10 12:11:11 -0700

    shuffle files

--- service_a/run_tests.sh
+++ service_a/run_tests.sh
@@ -8,7 +8,7 @@ echo "Dependencies ready, running tests."
 sleep 1
 
 # integration test
-ruby basic_server_test.rb
+ruby spec/basic_server_test.rb
 BASIC_TEST_PASS="$?"
 
 echo "Stopping dependencies."
@@ -17,7 +17,7 @@ docker-compose down
 # liveness probe test
 docker run -d -p 3000:8080 "$(docker build . | grep 'Successfully built' | awk '{print $3}')" > /dev/null
 PID="$(docker ps | grep basic_server | awk '{print $1}')"
-ruby readiness_probe_server_test.rb
+ruby spec/readiness_probe_server_test.rb
 LIVENESS_TEST_PASS="$?"
 
 echo "Basic tests pass? $BASIC_TEST_PASS"
